faaf114c685861fc5b5d49c82813bd4c07efcb8c
Bhavin Kapadia <bkapadia@touchbistro.com>
2024-01-01 19:11:52 -0500

    Initial Commit

---
+++ SwiftUI_SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_SwiftData
+//
+//  Created by Bhavin Kapadia on 2024-01-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_SwiftData/SwiftUI_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SwiftDataApp.swift
+//  SwiftUI_SwiftData
+//
+//  Created by Bhavin Kapadia on 2024-01-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
